f4c4784ef0fb2734bfe69cb14bb5f5bc9b864db4
Nav <navyasuri@nyu.edu>
2018-11-09 17:57:45 +0400

    File restrucutring

--- Flask-App/main-app.py
+++ Flask-App/main-app.py
@@ -1,9 +1,14 @@
 from flask import Flask, request
+import Utils.insert_db
 
 app = Flask(__name__)
 
 @app.route('/add-to-db')
 def addToDb():
     
-    
+    # Construct args here
+
+    # Use addToDatabase(netid, fname, pname, things, alrmet, year, fb, ig, wa)
+
+    # Construct return message based on success of insertion
     return "Success"
