932acf997fbba3a1948275ffbea7196aca2e5242
Nav <navyasuri@nyu.edu>
2018-11-10 01:29:15 +0400

    Main logic works on db

--- Flask-App/Utils/main_logic.py
+++ Flask-App/Utils/main_logic.py
@@ -1,6 +1,8 @@
-import get_from_db
-import insert_db
+import insert_db, get_from_db, update_db, sendemail
 import random
+
+subject = 'ConnectNYUAD: Match for this Week'
+message = '''Typing the message here'''
 
 def get_match_pairs():
     allUsers = get_from_db.getFromDatabase()
@@ -12,22 +14,42 @@ def get_match_pairs():
     # Make a list to store ones who are already matched
     already_matched = []
 
+    print "All Ids", all_ids
+
     # Iterate over all the ids 
-    for nid in all_ids:
+    for person in all_ids:
+
+        # Skip current iteration if person already present
+        if person in already_matched:
+            continue
 
         # Get the ids you shouldn't match to, then construct a list of valid ids to match with
-        dont_match = allUsers[nid]['already_met'].split(',')
-        match_ok = [item for item in all_ids not in dont_match]
-        match_ok = [item for item in match_ok not in already_matched]
+        p1_dont_match = allUsers[person]['already_met'].split(',')
+        match_ok = []
+        match_ok = [item for item in all_ids if item not in p1_dont_match]
+        match_ok = [item for item in match_ok if item not in already_matched]
 
         # Pick the first person from the valid list, add both to the list of already matched people
         second_person = match_ok[0]
-        already_matched.append(nid)
+        already_matched.append(person)
         already_matched.append(second_person)
 
         # Update the database to update each user's already met list
         p2_dont_match = allUsers[second_person]['already_met'].split(',')
-        updateDatabase(nid,dont_match, second_person, p2_dont_match)
+
+        # Update each user's dontmatch list to not repeat matching
+        p2_dont_match.append(person)
+        p1_dont_match.append(second_person)
+
+        update_db.updateDatabase(person, p1_dont_match, second_person, p2_dont_match)
+
+        # Fire Email function here
+        # sendemail.send_email(person, second_person, subject, message)
+
+        print("Person 1", person)
+        print("Person 2", second_person)
+
+
 
 
 
