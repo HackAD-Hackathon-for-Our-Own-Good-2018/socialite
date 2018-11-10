ac52e64374bed269d9edc5d42bcdec4db24c2839
Nav <navyasuri@nyu.edu>
2018-11-10 04:32:58 +0400

    Taking in form data to add to database

--- Flask-App/main-app.py
+++ Flask-App/main-app.py
@@ -21,6 +21,22 @@ def addToDb():
     print request.form.to_dict()
     # print request.form['net_id']
 
+    netid = request.form['net_id']
+    things = request.form['3_things']
+    fname = request.form['full_name']
+    pname = request.form['pref_name']
+    alrmet = request.form['already_met']
+    year = request.form['year']
+
+    ig = False
+    ig = True if 'check_ig' in request.form
+    fb = False
+    fb = True if 'check_fb' in request.form
+    wa = False
+    wa = True if 'check_wa' in request.form
+
+
+
     print "success"
     
     # Construct args here
