185ad81c4d328da208a8dd806846e16e97fb67e0
Nav <navyasuri@nyu.edu>
2018-11-10 06:28:10 +0400

    Added many changes for final prototype

--- Flask-App/Utils/main_logic.py
+++ Flask-App/Utils/main_logic.py
@@ -1,8 +1,48 @@
 import db_helpers, sendemail
 import random
 
+platforms = ['facebook/messenger', 'instagram', 'whatsapp']
 subject = 'ConnectNYUAD: Match for this Week'
-message = '''Typing the message here'''
+message2 = '''
+Dear {fname1} and {fname2}!
+
+You have been matched for this week!
+
+In the upcoming week, you have the opportunity to reach out and get to know each other. You can reach out to each other via email (just reply to this thread){platforms}. Don't be afraid to start the conversation - your match is just as willing to meet new people as you are! 
+
+Every person has three things that they want others to know These might turn out to be good conversation starters to move past the generic questions. The three things you want each other to know are:
+{pname1} - {things1}
+{pname2} - {things2}
+
+Be creative! - Ask people their hobbies and passions, or tell them yours. You are bound to have something in common! Be nice and open-minded. Kindness and mutual respect are not only the cornerstones of the NYUAD spirit, but also of any good friendship. 
+Good luck!
+
+Warm Regards
+ConnectNYUAD team
+
+(THIS IS A TEST! Thanks for bearing with us. -The Gabrus)
+'''
+
+message3 = '''
+Dear {fname1}, {fname2} and {fname3},
+
+You have been matched for this week!
+
+In the upcoming week, you have the opportunity to reach out and get to know each other. You can either do this individually or meet as a group. You can reach out to each other via email (reply to this thread){platforms}. Don't be afraid to start the conversation - your match is just as willing to meet new people as you are! 
+
+Every person has three things that they want others to know. These might turn out to be good conversation starters to move past the generic questions. The three things you want each other to know are:
+{pname1} - {things1}
+{pname2} - {things2}
+{pname3} - {things3}
+
+Be creative! - Ask people their hobbies and passions, or tell them yours. You are bound to have something in common! Be nice and openminded! Kindness and mutual respect are not only the cornerstones of the NYUAD spirit, but also of any good friendship. 
+Good luck!
+
+Warm regards,
+ConnectNYUAD team
+
+(THIS IS A TEST! Thanks for bearing with us. -The Gabrus)
+'''
 
 def get_match_pairs():
     allUsers = db_helpers.getFromDatabase()
@@ -49,6 +89,35 @@ def get_match_pairs():
         all_ids.remove(p2)
         all_ids.remove(p3)
 
+        platform = ''
+        p1_plat = [int(allUsers[p1]['check_fb']), int(allUsers[p1]['check_ig']), int(allUsers[p1]['check_wa'])]
+        p2_plat = [int(allUsers[p2]['check_fb']), int(allUsers[p2]['check_ig']), int(allUsers[p2]['check_wa'])]
+        p3_plat = [int(allUsers[p3]['check_fb']), int(allUsers[p3]['check_ig']), int(allUsers[p3]['check_wa'])]
+        
+        platList = []
+        for i in range(3):
+            if p1_plat[i] and p2_plat[i] and p3_plat:
+                platList.append(platforms[i])
+
+        if platList:
+            platform=", or "+", ".join(platList)
+
+        # Email
+        msg = message3.format(
+            fname1 = allUsers[p1]['full_name'],
+            fname2 = allUsers[p2]['full_name'],
+            fname3 = allUsers[p3]['full_name'], 
+            platforms=platform,
+            pname1 = allUsers[p1]['pref_name'],
+            things1 = allUsers[p1]['3_things'], 
+            pname2 = allUsers[p2]['pref_name'],
+            things2 = allUsers[p2]['3_things'], 
+            pname3 = allUsers[p3]['pref_name'],
+            things3 = allUsers[p3]['3_things']
+        )
+        # print(msg)
+        sendemail.send_email([p1,p2,p3], subject, msg)
+
 
     # Iterate over all the ids 
     for person in all_ids:
@@ -78,10 +147,33 @@ def get_match_pairs():
         db_helpers.updateDatabase(person, p1_dont_match)
         db_helpers.updateDatabase(second_person, p2_dont_match)
 
-        # Fire Email function here
-        # sendemail.send_email(person, second_person, subject, message)
+        platform = ''
+        p1_plat = [int(allUsers[person]['check_fb']), int(allUsers[person]['check_ig']), int(allUsers[person]['check_wa'])]
+        p2_plat = [int(allUsers[second_person]['check_fb']), int(allUsers[second_person]['check_ig']), int(allUsers[second_person]['check_wa'])]
+        
+        platList = []
+        for i in range(3):
+            if p1_plat[i] and p2_plat[i]:
+                platList.append(platforms[i])
 
-        print("Person 1", person)
-        print("Person 2", second_person)
+        if platList:
+            platform=", or "+", ".join(platList)
+
+        # Fire Email function here
+        msg = message2.format(
+            fname1 = allUsers[person]['full_name'],
+            fname2 = allUsers[second_person]['full_name'],
+            platforms=platform,
+            pname1 = allUsers[person]['pref_name'],
+            things1 = allUsers[person]['3_things'], 
+            pname2 = allUsers[second_person]['pref_name'], 
+            things2 = allUsers[second_person]['3_things']
+        )
+
+        # print(msg)
+        sendemail.send_email([person, second_person], subject, msg)
+
+        # print("Person 1", person)
+        # print("Person 2", second_person)
 
 get_match_pairs()
--- Flask-App/Utils/sendemail.py
+++ Flask-App/Utils/sendemail.py
@@ -1,19 +1,22 @@
 import smtplib
 from email.mime.text import MIMEText as text
 import emailconfig
-def send_email(User1,User2,subject, msg):
+def send_email(users, subject, msg):
+	for i in range(len(users)):
+		users[i] = users[i]+"@nyu.edu"
+
 	try:
-		message = '''Subject: {sub}\nFrom: {fro}\nTo: {u1}, {u2}\n{msg}'''.format(sub = subject, fro = emailconfig.EmailAddress, u1 = User1, u2 = User2, msg = msg)
+		message = '''Subject: {sub}\nFrom: {fro}\nTo: {users}\n{msg}'''.format(sub = subject, fro = emailconfig.EmailAddress, users = ", ".join(users), msg = msg)
 		server = smtplib.SMTP('smtp.gmail.com',587)
 		server.ehlo()
 		server.starttls()
 		server.login(emailconfig.EmailAddress, 
 		emailconfig.Password)
 		
-		server.sendmail(emailconfig.EmailAddress,[User1,User2],message)
+		server.sendmail(emailconfig.EmailAddress,users,message)
 		server.quit()
 		print('Email Sent')
-	except:
-		print('Email failed')
+	except (smtplib.SMTPAuthenticationError, smtplib.SMTPHeloError, smtplib.SMTPConnectError, smtplib.SMTPDataError) as e:
+		print('Email failed', e)
 
 
